dc331954aabe1aedb99314b3f8bdf7f840422e61
Samorodov Sergey <samorodovsv@yandex.ru>
2022-12-15 19:20:10 +0300

    Добавил 4 задачу

---
+++ src/Seminar2_Tasks/Task_4.java
@@ -0,0 +1,23 @@
+package Seminar2_Tasks;
+import java.util.*;
+
+/*
+Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+ Пользователю должно показаться сообщение, что пустые строки вводить нельзя. (try быть не должно)
+*/
+
+
+public class Task_4 {
+    public static void main(String[] args) {
+        System.out.print("Введите строку: ");
+        Scanner scr = new Scanner(System.in);
+        String str = scr.nextLine();
+        str = str.trim();
+        if(str.isEmpty()) {
+            scr.close();
+            throw new IllegalArgumentException("Пустую строку вводить нельзя.");
+        }
+        else
+            System.out.println(str);
+    }
+}
